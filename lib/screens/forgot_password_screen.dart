5924df6359d0b99d14818f37ce097da66750e69d
annisabalqisbalqis79-gif <annisabalqisbalqis79@gmail.com>
2026-06-04 21:15:34 +0700

    Upgrade logic forgot password and otp

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -951,12 +951,7 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
             Navigator.pop(context);
           },
           style: ElevatedButton.styleFrom(
-            backgroundColor: const Color.fromARGB(
-              255,
-              10,
-              128,
-              57,
-            ), // Green button
+            backgroundColor: AppColors.tosca.withOpacity(0.24), // Green button
             foregroundColor: Colors.white,
             minimumSize: const Size(double.infinity, 50),
             shape: RoundedRectangleBorder(
